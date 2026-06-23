35ea65fc8ab487566065bcbb25b3089d455e5b44
amiradc <78017279+amiradc@users.noreply.github.com>
2026-06-23 06:15:02 +0330

    Delete: Remove root Encryptor.java - not needed for runtime decryption

--- Encryptor.java
+++ Encryptor.java
@@ -1,3 +1,2 @@
-// ⚠️ توجه: این فایل باید حذف شود!
-// کانفیگ و کلید‌های حساس باید در کد سخت‌کد نشوند
-// فایل درست در app/src/main/java/com/fandogh/shekan/Encryptor.java قرار دارد
+# This file has been deleted
+# Use app/src/main/java/com/fandogh/shekan/Encryptor.java instead
